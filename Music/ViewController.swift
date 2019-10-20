b3e661667d961caa7646166392287612de4e4ebf
horechek <horechek@gmail.com>
2019-10-20 22:31:39 +0300

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Artem Kovardin on 20.10.2019.
+//  Copyright © 2019 Artem Kovardin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
